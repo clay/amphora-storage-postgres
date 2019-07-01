65bcd09f8a8f51f086944a71c30b37a546eea2e8
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2019-07-01 16:36:36 -0400

    Fixes typo in republished_at migration script

--- services/migrations/004_add_republished_at_column.sql
+++ services/migrations/004_add_republished_at_column.sql
@@ -2,7 +2,7 @@ ALTER TABLE IF EXISTS pages
 ADD COLUMN IF NOT EXISTS republished_at TIMESTAMPTZ;
 
 UPDATE pages
-SET published_at = subquery.republish_time
+SET republished_at = subquery.republish_time
 FROM (SELECT id, TO_TIMESTAMP(meta ->> 'publishTime', 'YYYY-MM-DD HH24:MI:SSZ') AS republish_time
   FROM pages
   WHERE meta IS NOT NULL
